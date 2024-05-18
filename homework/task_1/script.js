e755f54ec689530aeaed4ce86c79c32a0c2d5899
Elinur10 <164619785+Elinur10@users.noreply.github.com>
2024-05-18 05:24:05 +0400

    feat: introduction

---
+++ homework/task_1/script.js
@@ -0,0 +1,74 @@
+////////////// TASK 1 //////////////
+
+// 1. console.log(12 * 8);  //96
+
+// 2. console.log(10 / 2);  //5
+
+// 3. let num1 = 20
+// let num2 = 17
+// console.log(num1 + num2);  //37
+
+// 4.let name = 'Elnur'
+// let surname = 'Khalilov'
+// let year = '2004'
+// let info = ` ${name}, ${surname}, ${year} `
+// console.log(info);   //Elnur, Khalilov, 2004
+
+// 5.console.log(17 % 12);  //5
+
+// 6. let cityName = 'Baku'
+// cityName = 'Gence'
+// console.log(cityName); //Gence
+
+////// TASK 2 //////////////
+
+// let a = 1
+// let b = 1
+// let c = ++a
+// let d = b++
+// console.log(c); //2
+// console.log(d); //1
+// console.log(b); //2
+
+
+////////////// TASK 3 //////////////
+
+// let a = 2
+// let x = 1 + (a *= 2)
+// console.log(x); //5
+
+////////////// TASK 4 //////////////
+
+// console.log('test1', test);   // error
+// {
+//     let test = "something"
+//     console.log('test2', test); // test2 something
+// }
+// console.log('test3', test);  // error
+
+// console.log('test1', test);  // test1 undefined
+// {
+//     var test = "something"
+//     console.log('test2', test); // test2 something
+// }
+// console.log('test3', test); // test3 something
+
+////////////// TASK 5 //////////////
+
+// let name = "Ulfat"
+// console.log(typeof name); //string
+// let surname = Zakirli
+// console.log(typeof surname); //error
+// let year = 2000
+// console.log(typeof year); //number
+// year = "2000"
+// console.log(typeof year);  //string
+// let city;
+// console.log(typeof city); //undefined
+// let qualification = null
+// console.log(typeof qualification); //object
+// let obj = { name: 'ulfat' }
+// console.log(typeof obj);  //object
+// let arr = ['a', 'b', 'c']
+// console.log(typeof arr); //object
+
